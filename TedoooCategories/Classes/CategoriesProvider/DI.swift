2423cfcbc96e66ef3ca2374fecf394c7b3fb0b6a
morapelker <morapelker@gmail.com>
2022-07-02 20:00:12 +0300

    "v1.0.4"

--- TedoooCategories/Classes/CategoriesProvider/DI.swift
+++ TedoooCategories/Classes/CategoriesProvider/DI.swift
@@ -4,6 +4,8 @@ public class CategoriesDIContainer {
     
     private var container: Container!
     
+    public static let shared = CategoriesDIContainer()
+    
     public func registerContainer(container: Container) {
         self.container = container
         container.register(CategoriesProvider.self) { _ in
